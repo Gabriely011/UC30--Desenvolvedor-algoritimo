81811c0ae284c47f441fcf9bddd5417e1ef2f1e2
Gabriely <gabizinha090399@gmail.com>
2026-02-25 14:40:49 -0300

    Add files via upload

---
+++ UC- algoritimo 25.02.26 PYTHON/tipagem.py
@@ -0,0 +1,11 @@
+nome="Ada"
+print(type(nome))
+
+idade = 10
+print(type(idade))
+
+nota = 9.99
+print(type(idade))
+
+exemplo= True
+print(type(exemplo))
---
+++ UC- algoritimo 25.02.26 PYTHON/variaveis.py
@@ -0,0 +1,6 @@
+var1="gaby"
+Var1=17
+
+print(var1)
+print(Var1)
+# ele imprimiu
