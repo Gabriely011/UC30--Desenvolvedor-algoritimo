f16e83475ab9c8bd77c4ce21927069241dd964c8
Gabriely <gabizinha090399@gmail.com>
2026-02-26 14:38:19 -0300

    Add files via upload

---
+++ UC-algaritmo 26.02.26/calculo.py
@@ -0,0 +1,6 @@
+base= float(input("digite o primeiro valor:"))
+altura= float (input("Digite o primeiro valor:"))
+
+area = base * altura
+
+print(area)
---
+++ UC-algaritmo 26.02.26/exemplo.py
@@ -0,0 +1,7 @@
+aluno = input("Digite seu nome:")
+periodo=  int(input("digite em que período você está:"))
+ira= float(input("Digite seu ira"))
+
+print(type(aluno))
+print(type(periodo))
+print(type(ira))
