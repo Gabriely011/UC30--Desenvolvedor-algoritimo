6a12841bbabae089cf93f292cbfe8220544e1511
Gabriely <gabizinha090399@gmail.com>
2026-03-05 14:40:03 -0300

    Add files via upload

---
+++ UC-algoritmo 05.03.26/atividade1.py
@@ -0,0 +1,8 @@
+numeros = [80,20,20,40,90,50]
+print("Listas", numeros)
+
+print("Listas:", len(numeros))
+print("quantas vezes o 20 aparece", numeros.count(20))
+print("índice de 30:", numeros.index(30))
+ 
+
